fa3731c1a9d45ffe0cff02ccff740e1b38cd3982
moore-jt <moo23013@byui.edu>
2026-01-25 19:45:00 -0700

    Implement job and resume functionality in Program.cs
    Added job and resume classes to display job history.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Amazon";
+        job1._startYear = 2015;
+        job1._endYear = 2022;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Product Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Joseph Moore";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        
+        myResume.DisplayResume();
+        
+
+
+
     }
 }
