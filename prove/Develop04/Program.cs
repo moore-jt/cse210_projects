cbb6561a5f983fbb6c7f1443e0eccc3722e0b78a
moore-jt <moo23013@byui.edu>
2026-02-28 22:32:19 -0700

    Implement menu for activity selection

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Net.Quic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int selection;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.WriteLine("Select a choice from the menu: ");
+            
+            selection = int.Parse(Console.ReadLine());
+            if (selection == 1)
+            {
+                BreathingActivity activity = new BreathingActivity();
+                activity.Run();
+            }
+            else if (selection == 2)
+            {
+                ReflectionActivity activity = new ReflectionActivity();
+                activity.Run();
+            }
+            else if (selection == 3)
+            {
+                ListingActivity activity = new ListingActivity();
+                activity.Run();
+            }
+            else if (selection == 4)
+            {
+                break;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid Input. Please try again.");
+            }
+        }
     }
 }
