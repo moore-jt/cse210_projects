68d56482ffb6b3244d55acab5d96d74bbca6150e
moore-jt <moo23013@byui.edu>
2026-03-05 22:56:33 -0700

    Add shape classes and display their areas

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Purple", 2);
+        shapes.Add(shape1);
+
+        Circle shape2 = new Circle("Green", 4);
+        shapes.Add(shape2);
+
+        Rectangle shape3 = new Rectangle("Orange", 2, 2.5);
+        shapes.Add(shape3);
+
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area} units.");
+
+        }
     }
 }
